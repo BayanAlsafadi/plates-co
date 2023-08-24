8a3273968fb48b9bb3150c7e19522869bb2ac242
BayanAlsafadi <Bay.Alsafadi@gmail.com>
2023-08-25 01:47:44 +0300

    Build receipt ui and functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,51 @@
+import CartList from "./components/CartList/CartList";
+import Receipt from "./components/Receipt/Receipt";
+import { useState } from "react";
+import "./App.css";
 import { AppContext } from "./AppContext";
+
 const App = () => {
+  const [total, setTotal] = useState(0);
+  const [subTotal, setSubTotal] = useState(0);
+  const [deliveryCost, setDeliveryCost] = useState(4.95);
+  const [discount, setDiscount] = useState(0);
+  const [hasManyRedPlates, setHasManyRedPlates] = useState(false);
+  const [data, setData] = useState([
+    {
+      id: 1,
+      name: "Red Plate",
+      code: "R01",
+      price: 32.95,
+      quantity: 0,
+      src: "https://www.gompels.co.uk/image/cache/data/31500-1500x1500.webp",
+      total: 0,
+      subPrice: 32.95,
+      subTotal: 0,
+    },
+    {
+      id: 2,
+      name: "Green Plate",
+      code: "G01",
+      price: 24.95,
+      quantity: 0,
+      src: "https://www.gompels.co.uk/image/cache/data/54940-400x400.webp",
+      total: 0,
+      subPrice: 24.95,
+      subTotal: 0,
+    },
+    {
+      id: 3,
+      name: "Blue Plate",
+      code: "B01",
+      price: 7.95,
+      quantity: 0,
+      src: "https://www.gompels.co.uk/image/cache/data/57157-400x400.webp",
+      total: 0,
+      subPrice: 7.95,
+      subTotal: 0,
+    },
+  ]);
+
   return (
     <div className="App">
       <AppContext.Provider
@@ -18,9 +64,11 @@ const App = () => {
           setData,
         }}
       >
+        <CartList />
+        <Receipt />
       </AppContext.Provider>
     </div>
   );
 };
 
-export default App; 
+export default App;
--- src/components/Receipt/Receipt.jsx
+++ src/components/Receipt/Receipt.jsx
@@ -1,2 +1,54 @@
+import React, { useContext, useEffect } from "react";
+import { FcCheckmark, FcAdvertising } from "react-icons/fc";
+import "./Receipt.css";
+import { AppContext } from "../../AppContext";
+
+const Receipt = () => {
   const { total, subTotal, deliveryCost, discount, hasManyRedPlates } =
     useContext(AppContext);
+
+  return (
+    <div className="container">
+      <div className="details-container">
+        <div className="info-box">
+          <p>Discount</p>
+          <p>${discount}</p>
+        </div>
+        <div className="info-box">
+          <p>Delivery</p>
+          <p>${deliveryCost}</p>
+        </div>
+        <div className="info-box">
+          <p>Sub total</p>
+          <p>${subTotal}</p>
+        </div>
+        <div className="info-box">
+          <p>Total</p>
+          <p>${total}</p>
+        </div>
+      </div>
+      <div className="offers-container">
+        <div className="offers-info">
+          <p>
+            {total >= 50 ? <FcCheckmark /> : <FcAdvertising />}
+            Buy more than $50 and get $2.95 delivery fees!
+          </p>
+          <p>
+            {total >= 90 ? <FcCheckmark /> : <FcAdvertising />}
+            Buy more than $90 and get a free delivery!
+          </p>
+
+          <p>
+            {hasManyRedPlates ? <FcCheckmark /> : <FcAdvertising />}
+            Buy one red plate, get the second half price”.
+          </p>
+        </div>
+        <div>
+          <button className="checkout-btn">checkout</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Receipt;
